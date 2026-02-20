0d60d587b677128fb7dcdc96c51b7b01e6d8c45f
Ttibsi <ashisbitt@icloud.com>
2026-02-20 08:19:28 +0000

    rename exec to vega

--- nob.c
+++ nob.c
@@ -1,12 +1,14 @@
 #define NOB_IMPLEMENTATION
 #include "include/nob.h"
 
+const char* exec_name = "vega";
+
 int main(int argc, char* argv[]) {
     GO_REBUILD_URSELF(argc, argv);
 
     Cmd cmd = {0};
     cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Iinclude", "-Linclude", "-lm");
-    cmd_append(&cmd, "-o", "main");
+    cmd_append(&cmd, "-o", exec_name);
     cmd_append(&cmd, "src/main.c");
 
     if (!cmd_run(&cmd)) { return 1; }
