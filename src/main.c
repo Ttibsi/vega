12e3be252262e2c8e69ce7cf6975d4be6ee0fa0d
Ttibsi <ashisbitt@icloud.com>
2026-02-20 08:17:36 +0000

    add comments

--- src/main.c
+++ src/main.c
@@ -9,10 +9,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Load image
     int dim_x, dim_y, channels; // out params
     unsigned char* data = stbi_load(argv[1], &dim_x, &dim_y, &channels, 1);
     const int data_len = dim_x * dim_y;
 
+    // Draw image to terminal
     printf("X: %d, Y: %d, chann: %d\n", dim_x, dim_y, channels);
     for (int y = 0; y < dim_y; y++) {
         for (int x = 0; x < dim_x; x++) {
